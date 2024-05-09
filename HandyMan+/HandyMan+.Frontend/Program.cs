08973f04dbbe793027affab92653a30631144b8b
ARNALDO RAFAEL CASTILLA FLOREZ <arnaldocastilla1134881@correo.itm.edu.co>
2024-05-09 13:59:35 -0500

    add template matblazor

--- HandyMan+/HandyMan+.Frontend/Program.cs
+++ HandyMan+/HandyMan+.Frontend/Program.cs
@@ -1,6 +1,7 @@
 using CurrieTechnologies.Razor.SweetAlert2;
 using HandyMan_.Frontend;
 using HandyMan_.Frontend.Repositories;
+using MatBlazor;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -8,8 +9,13 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.AddMatBlazor();
+
+
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5271/") }); 
 builder.Services.AddScoped<IRepository, Repository>();
+
 builder.Services.AddSweetAlert2();
+
 
 await builder.Build().RunAsync();
