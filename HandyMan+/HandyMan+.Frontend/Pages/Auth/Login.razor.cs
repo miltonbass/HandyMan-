e32a30bc62169a671a3c57dafdbe9ceb971c6e00
Isra <isratecno@gmail.com>
2024-05-13 13:17:38 -0500

    AUTH-FEAT: Login/Register Base

---
+++ HandyMan+/HandyMan+.Frontend/Pages/Auth/Login.razor.cs
@@ -0,0 +1,45 @@
+using CurrieTechnologies.Razor.SweetAlert2;
+using HandyMan_.Frontend.Repositories;
+using HandyMan_.Shered.DTOs;
+using Microsoft.AspNetCore.Components;
+using Orders.Frontend.Services;
+using Orders.Shared.DTOs;
+
+namespace HandyMan_.Frontend.Pages.Auth
+{
+    public partial class Login
+    {
+        private LoginDTO loginDTO = new();
+        private bool wasClose;
+
+        [Inject] private NavigationManager NavigationManager { get; set; } = null!;
+        [Inject] private SweetAlertService SweetAlertService { get; set; } = null!;
+        [Inject] private IRepository Repository { get; set; } = null!;
+        [Inject] private ILoginService LoginService { get; set; } = null!;
+
+        private async Task CloseModalAsync()
+        {
+         
+        }
+
+        private async Task LoginAsync()
+        {
+            if (wasClose)
+            {
+                NavigationManager.NavigateTo("/");
+                return;
+            }
+
+            var responseHttp = await Repository.PostAsync<LoginDTO, TokenDTO>("/api/accounts/Login", loginDTO);
+            if (responseHttp.Error)
+            {
+                var message = await responseHttp.GetErrorMessageAsync();
+                await SweetAlertService.FireAsync("Error", message, SweetAlertIcon.Error);
+                return;
+            }
+
+            await LoginService.LoginAsync(responseHttp.Response!.Token);
+            NavigationManager.NavigateTo("/");
+        }
+    }
+}
---
+++ HandyMan+/HandyMan+.Frontend/Pages/Auth/Register.razor.cs
@@ -0,0 +1,113 @@
+﻿using CurrieTechnologies.Razor.SweetAlert2;
+using HandyMan_.Frontend.Repositories;
+using HandyMan_.Shered.Entities;
+using Microsoft.AspNetCore.Components;
+using Orders.Frontend.Services;
+using Orders.Shared.DTOs;
+using Orders.Shared.Enums;
+
+namespace HandyMan_.Frontend.Pages.Auth
+{
+    public partial class Register
+    {
+        private UserDTO userDTO = new();
+        private List<Country>? countries;
+        private List<State>? states;
+        private List<City>? cities;
+        private bool loading;
+        private string? imageUrl;
+
+        [Inject] private NavigationManager NavigationManager { get; set; } = null!;
+        [Inject] private SweetAlertService SweetAlertService { get; set; } = null!;
+        [Inject] private IRepository Repository { get; set; } = null!;
+        [Inject] private ILoginService LoginService { get; set; } = null!;
+
+        protected override async Task OnInitializedAsync()
+        {
+            await LoadCountriesAsync();
+        }
+
+        private void ImageSelected(string imagenBase64)
+        {
+            userDTO.Photo = imagenBase64;
+            imageUrl = null;
+        }
+
+
+        private async Task LoadCountriesAsync()
+        {
+            var responseHttp = await Repository.GetAsync<List<Country>>("/api/countries/combo");
+            if (responseHttp.Error)
+            {
+                var message = await responseHttp.GetErrorMessageAsync();
+                await SweetAlertService.FireAsync("Error", message, SweetAlertIcon.Error);
+                return;
+            }
+
+            countries = responseHttp.Response;
+        }
+
+        private async Task CountryChangedAsync(ChangeEventArgs e)
+        {
+            var selectedCountry = Convert.ToInt32(e.Value!);
+            states = null;
+            cities = null;
+            userDTO.CityId = 0;
+            await LoadStatesAsyn(selectedCountry);
+        }
+
+        private async Task LoadStatesAsyn(int countryId)
+        {
+            var responseHttp = await Repository.GetAsync<List<State>>($"/api/states/combo/{countryId}");
+            if (responseHttp.Error)
+            {
+                var message = await responseHttp.GetErrorMessageAsync();
+                await SweetAlertService.FireAsync("Error", message, SweetAlertIcon.Error);
+                return;
+            }
+
+            states = responseHttp.Response;
+        }
+
+        private async Task StateChangedAsync(ChangeEventArgs e)
+        {
+            var selectedState = Convert.ToInt32(e.Value!);
+            cities = null;
+            userDTO.CityId = 0;
+            await LoadCitiesAsyn(selectedState);
+        }
+
+        private async Task LoadCitiesAsyn(int stateId)
+        {
+            var responseHttp = await Repository.GetAsync<List<City>>($"/api/cities/combo/{stateId}");
+            if (responseHttp.Error)
+            {
+                var message = await responseHttp.GetErrorMessageAsync();
+                await SweetAlertService.FireAsync("Error", message, SweetAlertIcon.Error);
+                return;
+            }
+
+            cities = responseHttp.Response;
+        }
+
+
+        private async Task CreteUserAsync()
+        {
+            userDTO.UserName = userDTO.Email;
+            userDTO.UserType = UserType.User;
+            loading = true;
+            var responseHttp = await Repository.PostAsync<UserDTO>("/api/accounts/CreateUser", userDTO);
+            loading = false;
+
+            if (responseHttp.Error)
+            {
+                var message = await responseHttp.GetErrorMessageAsync();
+                await SweetAlertService.FireAsync("Error", message, SweetAlertIcon.Error);
+                return;
+            }
+
+            await SweetAlertService.FireAsync("Confirmación", "Su cuenta ha sido creada con éxito. Se te ha enviado un correo electrónico con las instrucciones para activar tu usuario.", SweetAlertIcon.Info);
+            NavigationManager.NavigateTo("/");
+        }
+    }
+}
