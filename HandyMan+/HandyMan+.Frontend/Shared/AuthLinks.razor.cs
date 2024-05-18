7f63e675c976a9729104fdc8affc1f0d6ef97a06
ARNALDO RAFAEL CASTILLA FLOREZ <arnaldocastilla1134881@correo.itm.edu.co>
2024-05-17 21:22:14 -0500

    modal register user with validation c:

--- HandyMan+/HandyMan+.Frontend/Shared/AuthLinks.razor.cs
+++ HandyMan+/HandyMan+.Frontend/Shared/AuthLinks.razor.cs
@@ -8,14 +8,24 @@ using HandyMan_.Frontend.Repositories;
 using Orders.Frontend.Services;
 using Orders.Shared.DTOs;
 using Microsoft.AspNetCore.Components.Forms;
+using Orders.Shared.Enums;
+using System.Runtime.CompilerServices;
+using HandyMan_.Shered.Entities;
 
 namespace HandyMan_.Frontend.Shared
 {
     public partial class AuthLinks
     {
-        private EditContext editContext = null!;
+        private string CountryId = null!;
+        private string StateId = null!;
+        private UserDTO userDTO { get; set; } = new UserDTO();
+        private List<Country>? countries;
+        private List<State>? states;
+        private List<City>? cities;
+        private string? imageUrl;
+        private bool loading;
 
-        private LoginDTO loginDTO { get; set; } = new LoginDTO();
+        private LoginDTO loginDTO = new();
         [Inject] private IRepository Repository { get; set; } = null!;
         [Inject] private NavigationManager NavigationManager { get; set; } = null!;
 
@@ -23,17 +33,12 @@ namespace HandyMan_.Frontend.Shared
 
         private string? photoUser;
         private bool dialogIsOpen = false;
-        
+        private bool dialogIsOpenRegister = false;
 
         [CascadingParameter] private Task<AuthenticationState> AuthenticationStateTask { get; set; } = null!;
         [CascadingParameter] IModalService Modal { get; set; } = default!;
 
         [Inject] private SweetAlertService SweetAlertService { get; set; } = null!;
-
-        protected override void OnInitialized()
-        {
-            editContext = new(loginDTO);
-        }
 
         protected override async Task OnParametersSetAsync()
         {
@@ -44,6 +49,25 @@ namespace HandyMan_.Frontend.Shared
             {
                 photoUser = photoClaim.Value;
             }
+        }
+        
+        
+
+        protected override async Task OnInitializedAsync()
+        {
+            await LoadCountriesAsync();
+        }
+        private async Task LoadCountriesAsync()
+        {
+            var responseHttp = await Repository.GetAsync<List<Country>>("/api/countries/combo");
+            if (responseHttp.Error)
+            {
+                var message = await responseHttp.GetErrorMessageAsync();
+                await SweetAlertService.FireAsync("Error", message, SweetAlertIcon.Error);
+                return;
+            }
+
+            countries = responseHttp.Response;
         }
 
         private async Task LoginAsync()
@@ -63,14 +87,90 @@ namespace HandyMan_.Frontend.Shared
 
         }
 
-        private void ShowModal()
-        {
-            Modal.Show<Login>();
-        }
-
         void OpenDialog()
         {
             dialogIsOpen = true;
+        }
+        void OpenDialogRegister()
+        {
+            dialogIsOpenRegister = true;
+        }
+
+        private async Task CreteUserAsync()
+        {
+            userDTO.UserName = userDTO.Email;
+            userDTO.UserType = UserType.Costumer;
+            loading = true;
+            var responseHttp = await Repository.PostAsync<UserDTO>("/api/accounts/CreateUser", userDTO);
+            loading = false;
+
+            if (responseHttp.Error)
+            {
+                var message = await responseHttp.GetErrorMessageAsync();
+                await SweetAlertService.FireAsync("Error", message, SweetAlertIcon.Error);
+                return;
+            }
+
+            await SweetAlertService.FireAsync("Confirmación", "Su cuenta ha sido creada con éxito. Se te ha enviado un correo electrónico con las instrucciones para activar tu usuario.", SweetAlertIcon.Info);
+            NavigationManager.NavigateTo("/");
+        }
+
+        private async Task CountryChangedAsync(string e)
+        {
+            var selectedCountry = Convert.ToInt32(e);
+            states = null;
+            cities = null;
+            userDTO.CityId = 0;
+            await LoadStatesAsyn(selectedCountry);
+        }
+        private async Task LoadStatesAsyn(int countryId)
+        {
+            var responseHttp = await Repository.GetAsync<List<State>>($"/api/states/combo/{countryId}");
+            if (responseHttp.Error)
+            {
+                var message = await responseHttp.GetErrorMessageAsync();
+                await SweetAlertService.FireAsync("Error", message, SweetAlertIcon.Error);
+                return;
+            }
+
+            states = responseHttp.Response;
+        }
+        private async Task StateChangedAsync(string e)
+        {
+            var selectedState = Convert.ToInt32(e);
+            cities = null;
+            userDTO.CityId = 0;
+            await LoadCitiesAsyn(selectedState);
+        }
+
+        private async Task LoadCitiesAsyn(int stateId)
+        {
+            var responseHttp = await Repository.GetAsync<List<City>>($"/api/cities/combo/{stateId}");
+            if (responseHttp.Error)
+            {
+                var message = await responseHttp.GetErrorMessageAsync();
+                await SweetAlertService.FireAsync("Error", message, SweetAlertIcon.Error);
+                return;
+            }
+
+            cities = responseHttp.Response;
+        }
+
+
+        void CloseDialog() {
+            loginDTO = new();
+            dialogIsOpen = false;
+        }
+
+        void CloseDialogRegister() {
+            dialogIsOpenRegister = false;
+            userDTO = new();
+        }
+
+        private void ImageSelected(string imagenBase64)
+        {
+            userDTO.Photo = imagenBase64;
+            imageUrl = null;
         }
     }
 }
