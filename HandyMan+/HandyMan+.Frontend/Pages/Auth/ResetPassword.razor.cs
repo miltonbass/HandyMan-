88aaca5105da8a9b368229fed694b85b0977660a
lagm1290 <lagm1290@gmail.com>
2024-05-17 23:08:49 -0500

    reset password

---
+++ HandyMan+/HandyMan+.Frontend/Pages/Auth/ResetPassword.razor.cs
@@ -0,0 +1,38 @@
+﻿using Blazored.Modal.Services;
+using CurrieTechnologies.Razor.SweetAlert2;
+using HandyMan_.Frontend.Repositories;
+using HandyMan_.Shared.DTOs;
+using Microsoft.AspNetCore.Components;
+
+namespace HandyMan_.Frontend.Pages.Auth
+{
+    public partial class ResetPassword
+    {
+        private ResetPasswordDTO resetPasswordDTO = new();
+        private bool loading;
+
+        [Inject] private NavigationManager NavigationManager { get; set; } = null!;
+        [Inject] private SweetAlertService SweetAlertService { get; set; } = null!;
+        [Inject] private IRepository Repository { get; set; } = null!;
+        [Parameter, SupplyParameterFromQuery] public string Token { get; set; } = string.Empty;
+        [CascadingParameter] IModalService Modal { get; set; } = default!;
+
+        private async Task ChangePasswordAsync()
+        {
+            resetPasswordDTO.Token = Token;
+            loading = true;
+            var responseHttp = await Repository.PostAsync("/api/accounts/ResetPassword", resetPasswordDTO);
+            loading = false;
+            if (responseHttp.Error)
+            {
+                var message = await responseHttp.GetErrorMessageAsync();
+                await SweetAlertService.FireAsync("Error", message, SweetAlertIcon.Error);
+                loading = false;
+                return;
+            }
+
+            await SweetAlertService.FireAsync("Confirmación", "Contraseña cambiada con éxito, ahora puede ingresar con su nueva contraseña.", SweetAlertIcon.Info);
+            NavigationManager.NavigateTo("/Login");
+        }
+    }
+}
