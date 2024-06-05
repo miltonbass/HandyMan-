f8323ed322e4d3c1349e435d2ec00fd053fe73c7
ARNALDO RAFAEL CASTILLA FLOREZ <arnaldocastilla1134881@correo.itm.edu.co>
2024-06-04 22:03:43 -0500

    save data to db orders

--- HandyMan+/HandyMan+.Frontend/Pages/Cart/ShowCart.razor.cs
+++ HandyMan+/HandyMan+.Frontend/Pages/Cart/ShowCart.razor.cs
@@ -5,6 +5,7 @@ using HandyMan_.Frontend.Repositories;
 using HandyMan_.Shered.Entities;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Components;
+using HandyMan_.Shered.DTOs;
 
 namespace HandyMan_.Frontend.Pages.Cart
 {
@@ -14,6 +15,7 @@ namespace HandyMan_.Frontend.Pages.Cart
         private int counter = 0;
         private bool isAuthenticated;
         private bool loading;
+        public OrderDTO OrderDTO { get; set; } = new();
         public List<TemporalOrder>? temporalOrders { get; set; }
 
         [Inject] private NavigationManager NavigationManager { get; set; } = null!;
@@ -56,10 +58,80 @@ namespace HandyMan_.Frontend.Pages.Cart
             }
         }
 
+        private async Task ConfirmOrderAsync()
+        {
+            var result = await SweetAlertService.FireAsync(new SweetAlertOptions
+            {
+                Title = "Confirmación",
+                Text = "¿Esta seguro que quieres confirmar el pedido?",
+                Icon = SweetAlertIcon.Question,
+                ShowCancelButton = true
+            });
+
+            var confirm = string.IsNullOrEmpty(result.Value);
+            if (confirm)
+            {
+                return;
+            }
+
+            var httpActionResponse = await Repository.PostAsync("/api/orders", OrderDTO);
+            if (httpActionResponse.Error)
+            {
+                var message = await httpActionResponse.GetErrorMessageAsync();
+                await SweetAlertService.FireAsync("Error", message, SweetAlertIcon.Error);
+                return;
+            }
+
+            CloseCartModal();
+            await SweetAlertService.FireAsync("Confirmación", "Su peidido ha sido confirmado. En pronto se contactarán para materializar los servicios que contrató, muchas gracias.", SweetAlertIcon.Info);
+            //NavigationManager.NavigateTo("/Cart/OrderConfirmed");
+        }
+
+        private async Task Delete(int temporalOrderId)
+        {
+            var result = await SweetAlertService.FireAsync(new SweetAlertOptions
+            {
+                Title = "Confirmación",
+                Text = "¿Esta seguro que quieres borrar el registro?",
+                Icon = SweetAlertIcon.Question,
+                ShowCancelButton = true
+            });
+
+            var confirm = string.IsNullOrEmpty(result.Value);
+
+            if (confirm)
+            {
+                return;
+            }
+
+            var responseHttp = await Repository.DeleteAsync<TemporalOrder>($"api/temporalOrders/{temporalOrderId}");
+
+            if (responseHttp.Error)
+            {
+                if (responseHttp.HttpResponseMessage.StatusCode == System.Net.HttpStatusCode.NotFound)
+                {
+                    NavigationManager.NavigateTo("/");
+                    return;
+                }
+
+                var mensajeError = await responseHttp.GetErrorMessageAsync();
+                await SweetAlertService.FireAsync("Error", mensajeError, SweetAlertIcon.Error);
+                return;
+            }
+
+            await LoadAsync();
+            var toast = SweetAlertService.Mixin(new SweetAlertOptions
+            {
+                Toast = true,
+                Position = SweetAlertPosition.BottomEnd,
+                ShowConfirmButton = false,
+                Timer = 3000
+            });
+            await toast.FireAsync(icon: SweetAlertIcon.Success, message: "Producto eliminado del carro de compras.");
+        }
         private async Task CloseCartModal()
         {
             await BlazoredModal.CloseAsync(ModalResult.Ok());
         }
-
     }
 }
