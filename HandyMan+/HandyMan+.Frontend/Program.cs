54b5eab76a41153c70a77ab9875583b80fcbf1d2
ARNALDO RAFAEL CASTILLA FLOREZ <arnaldocastilla1134881@correo.itm.edu.co>
2024-06-08 03:27:20 -0500

    after publishin front

--- HandyMan+/HandyMan+.Frontend/Program.cs
+++ HandyMan+/HandyMan+.Frontend/Program.cs
@@ -19,7 +19,7 @@ builder.Services.AddScoped<AuthenticationProviderJWT>();
 builder.Services.AddScoped<AuthenticationStateProvider, AuthenticationProviderJWT>(x => x.GetRequiredService<AuthenticationProviderJWT>());
 builder.Services.AddScoped<ILoginService, AuthenticationProviderJWT>(x => x.GetRequiredService<AuthenticationProviderJWT>());
 
-var uriBack = "https://handymanplus.azurewebsites.net";
+var uriBack = "https://handymanplus.azurewebsites.net/";
 //var uriBack = "https://localhost:7002/";
 
 builder.Services.AddSingleton(sp => new HttpClient { BaseAddress = new Uri(uriBack) }); 
